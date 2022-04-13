1b66804f56751b02dfbafc9fae352dfafab15542
aleksandr <atrostyanko@gmail.com>
2022-04-13 20:46:18 +0300

    asd

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    private String title;
+
+    public Calculator(String title) {
+        this.title = title;
+    }
+
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
