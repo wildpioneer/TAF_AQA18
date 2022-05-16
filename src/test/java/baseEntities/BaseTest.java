5975a778861c23cbe6359ee15d7b106de55f2332
aleksandr <atrostyanko@gmail.com>
2022-05-16 21:31:43 +0300

    Totally Ready

--- src/test/java/baseEntities/BaseTest.java
+++ src/test/java/baseEntities/BaseTest.java
@@ -1,16 +1,32 @@
 package baseEntities;
 
+import com.google.gson.Gson;
+import models.Project;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import services.BrowsersService;
 import steps.LoginStep;
 import steps.NavigationStep;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class BaseTest {
     protected WebDriver driver;
     protected LoginStep loginStep;
     protected NavigationStep navigationStep;
+
+    protected Project mainProject;
+
+    @BeforeTest
+    public void prepareData() throws IOException {
+        Reader reader = Files.newBufferedReader(Paths.get("projectResult.json"));
+        mainProject = new Gson().fromJson(reader, Project.class);
+    }
 
     @BeforeMethod
     public void setup() {
---
+++ src/test/java/tests/ProjectTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import baseEntities.BaseTest;
+import org.testng.annotations.Test;
+
+public class ProjectTest extends BaseTest {
+
+    @Test
+    public void addProjectTest() {
+        System.out.println(mainProject.toString());
+    }
+}
